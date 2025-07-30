9911de5c943b4b0da587a0d93c4e7642d7d26f34
Turbo-Teflon <marwan.neuman@gmail.com>
2025-07-30 14:17:08 +0200

    S005 Mise en place

---
+++ bo/Ticket.js
@@ -0,0 +1,18 @@
+const EtatTicket = Object.freeze({
+    OUVERT: 'ouvert',
+    CLOS: 'clos',
+    TOUS: 'tous',
+});
+
+class Ticket {
+    constructor(id, titre, auteur, description, creation) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.description = description;
+        this.creation = creation;
+        this.etat = EtatTicket.OUVERT;
+    }
+}
+
+module.exports = { Ticket, EtatTicket };
