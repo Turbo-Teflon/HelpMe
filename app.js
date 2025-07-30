4fd8139eeff64e30ebcacb00e9cbcda94ce53295
Turbo-Teflon <marwan.neuman@gmail.com>
2025-07-30 13:52:51 +0200

    S004 TODO faire le footer et feuille de style

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 "use strict";
-require("dotenv").config();
 
+require("dotenv").config();
 const express = require("express");
 const app = express();
+let path = require('path');
+
 const port = process.env.PORT;
 
 console.log("port :" + port);
 
 // EJS
-app.set("views", "./views");
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.render("liste-tickets");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
